7ab6639a1545400bf900677ad6a9e66a13cee66f
GigolNft <GigolNft@gmail.com>
2022-03-23 20:43:34 +0100

    Entity follower

---
+++ migrations/Version20220323193438.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220323193438 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE follower (id INT AUTO_INCREMENT NOT NULL, tw_user_id VARCHAR(55) NOT NULL, tw_username VARCHAR(22) NOT NULL, tw_name LONGTEXT NOT NULL, tw_verified TINYINT(1) NOT NULL, wallet_eth VARCHAR(55) DEFAULT NULL, wallet_sol VARCHAR(55) DEFAULT NULL, favorite TINYINT(1) NOT NULL, create_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', update_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id), UNIQUE(tw_user_id, tw_username)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE follower');
+    }
+}
---
+++ src/Entity/Follower.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FollowerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FollowerRepository::class)]
+class Follower
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 55)]
+    private $twUserId;
+
+    #[ORM\Column(type: 'string', length: 22)]
+    private $twUsername;
+
+    #[ORM\Column(type: 'text')]
+    private $twName;
+
+    #[ORM\Column(type: 'boolean')]
+    private $twVerified;
+
+    #[ORM\Column(type: 'string', length: 55, nullable: true)]
+    private $walletEth;
+
+    #[ORM\Column(type: 'string', length: 55, nullable: true)]
+    private $walletSol;
+
+    #[ORM\Column(type: 'boolean')]
+    private $favorite;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $createAt;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $updateAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTwUserId(): ?string
+    {
+        return $this->twUserId;
+    }
+
+    public function setTwUserId(string $twUserId): self
+    {
+        $this->twUserId = $twUserId;
+
+        return $this;
+    }
+
+    public function getTwUsername(): ?string
+    {
+        return $this->twUsername;
+    }
+
+    public function setTwUsername(string $twUsername): self
+    {
+        $this->twUsername = $twUsername;
+
+        return $this;
+    }
+
+    public function getTwName(): ?string
+    {
+        return $this->twName;
+    }
+
+    public function setTwName(string $twName): self
+    {
+        $this->twName = $twName;
+
+        return $this;
+    }
+
+    public function getTwVerified(): ?bool
+    {
+        return $this->twVerified;
+    }
+
+    public function setTVerified(bool $twVerified): self
+    {
+        $this->twVerified = $twVerified;
+
+        return $this;
+    }
+
+    public function getWalletEth(): ?string
+    {
+        return $this->walletEth;
+    }
+
+    public function setWalletEth(?string $walletEth): self
+    {
+        $this->walletEth = $walletEth;
+
+        return $this;
+    }
+
+    public function getWalletSol(): ?string
+    {
+        return $this->walletSol;
+    }
+
+    public function setWalletSol(?string $walletSol): self
+    {
+        $this->walletSol = $walletSol;
+
+        return $this;
+    }
+
+    public function getFavorite(): ?bool
+    {
+        return $this->favorite;
+    }
+
+    public function setFavorite(bool $favorite): self
+    {
+        $this->favorite = $favorite;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeImmutable $createAt): self
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(?\DateTimeImmutable $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+}
